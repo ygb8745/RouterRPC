00715e54a768c9ccef5ea5248b922897e07274c7
YGB8745 <ygb8745@gmail.com>
2018-06-03 21:03:49 +0800

    done router part1

--- router.erl
+++ router.erl
@@ -8,6 +8,7 @@
 
 -define(undef, undefined).
 
+
 start() -> start_link().
 start_link() ->
     gen_server:start_link({local, router},%本地注册为router ,也可以通过whereis()函数来获得pid.
@@ -24,51 +25,74 @@ init(_Args) ->
 stop() ->
     gen_server:cast(router, stop).
 
-update_router()->
-    gen_server:call(router, update_router_start).
+terminate(normal, _State) ->
+    ok.
 
-handle_call(update_router_start, _From, State) ->
-    Ref = erlang:make_ref(),
-    % put(Ref, update_router_start),
-    % 每个节点返回的路由信息应该是 {本节点名, [本节点可达的节点列表]}
-    {RestList, _todo_BadNodes} = rpc:multicall(sen_server, call, [router,
-                                                                    {update_router_request, _KnowenNodeList = [node()|nodes()], Ref}]),
-    FlattenList = lists:flatten(RestList),
-    lists:foreach(
-        fun({NodeName, ConnectiongList})->
-            % 放在进程字典中的路由信息是  {router_item, 节点名} -> [该节点可达的节点列表]
-            put({router_item, NodeName}, ConnectiongList)
+update_router()->
+    gen_server:cast(router, update_router_start).
+
+handle_call({update_router_request, KnowenNodeList, Ref}, _From, State) ->
+    log({handle_call, {update_router_request, KnowenNodeList, Ref}}),
+    Reply =
+        case get(Ref) of
+            ?undef ->
+                put(Ref, update_router_start),% todo 删除Ref
+                % 检查与自己连接的节点是不是都在 KnowenNodeList中,如果有未知节点就向未知节点也发信.
+                UnKnowenNodeList = sets:to_list(sets:subtract(sets:from_list(nodes()),
+                                                 sets:from_list(KnowenNodeList))),
+                UnKnowenNodeRestList=
+                    case UnKnowenNodeList of
+                        []-> %do nothing
+                            [];
+                        _ ->
+                            {RestList, _todo_BadNodes} = rpc:multicall(gen_server, call, [router,
+                                                                            {update_router_request, UnKnowenNodeList, Ref}]),
+                            RestList
+                    end,
+                [{node(), nodes()} | UnKnowenNodeRestList];
+            _ ->
+            [] % ignore 已经处理过这个router请求了
         end,
-        FlattenList),
-    {reply, ok, State};
-handle_call({update_router_request, KnowenNodeList, Ref}, _From, OldState) ->
-    case get(Ref) of
-        ?undef ->
-            put(Ref, update_router_start),
-            % 检查与自己连接的节点是不是都在 KnowenNodeList中,如果有未知节点就向未知节点也发信.
-            UnKnowenNodeList = sets:subtract(sets:from_list(nodes()),
-                          sets:from_list(KnowenNodeList)),
-            UnKnowenNodeRestList=
-            case UnKnowenNodeList of
-                []-> %do nothing
-                    [];
-                _ ->
-                    {UnKnowenNodeRestList, _todo_BadNodes} = rpc:multicall(sen_server, call, [router,
-                                                                    {update_router_request, UnKnowenNodeList, Ref}]),
-                    UnKnowenNodeRestList
-            end,
-            [{node(), nodes()}|UnKnowenNodeRestList];
-        _ -> ignore %??
-    end;
+    {reply, Reply, State};
 handle_call(Request, _From, OldState) ->
-    io:format("router.unhandle_call:~p~n",[{Request, _From, OldState}]),
+    log({"router.unhandle_call:~p~n",[{Request, _From, OldState}]}),
     {reply, Request, OldState}. %{reply, Reply, State1}。Reply是需要回馈给客户端的答复，同时 State1 是gen_server的状态的新值。
 
+handle_cast(update_router_start, State) ->
+    spawn(fun()->
+        Ref = erlang:make_ref(),
+        % put(Ref, update_router_start),
+        % 每个节点的路由条目是: {节点名, [该节点可达的节点列表]}
+        % 每个节点返回的路由信息应该是 [本节点路由条目, 其他节点路由条目]
+        % multicall包括本节点.
+        % gen_server循环call的问题.
+        % 拼写错误 gen_server  sen_server.
+        {RestList, _todo_BadNodes} = rpc:multicall(gen_server, call, [router,
+                                                                        {update_router_request,
+                                                                        _KnowenNodeList = [node()|nodes()],
+                                                                        Ref}]),
+        % todo 处理{badrpc, Why}
+        FlattenList = lists:flatten(RestList),
+        gen_server:cast(router,{update_router_done, FlattenList})
+    end),
+    {noreply, State};
+handle_cast({update_router_done, FlattenList}, State) ->
+    lists:foreach(
+                fun({NodeName, ConnectiongList})->
+                    % 放在进程字典中的路由信息是  {router_item, 节点名} -> [该节点可达的节点列表]
+                    io:format("router item: ~p -> ~p~n",[{router_item, NodeName}, ConnectiongList]),
+                    put({router_item, NodeName}, ConnectiongList)
+                end,
+                FlattenList),
+    {noreply, State};
 handle_cast(stop, State) ->
     {stop, normal, State};
 handle_cast(_Request, OldState) ->
-    io:format("router.unhandle_call:~p~n",[{_Request, OldState}]),
+    log({"router.unhandle_call:~p~n",[{_Request, OldState}]}),
     {noreply, OldState}.
 
-terminate(normal, _State) ->
-    ok.
+handle_info(_Info,State)-> {noreply, State}.
+code_change(_OldVsn, State, _Extra)-> {ok, State}.
+
+log(What)->
+    io:format("Log who:~p-~p, ~nwhat:~p~n",[node(), self(), What]).
