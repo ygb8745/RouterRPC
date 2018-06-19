07e52234bad4b396c8d001b35538115feba50116
YGB8745 <ygb8745@gmail.com>
2018-06-19 22:32:16 +0800

    M code:load_file

--- router_code.erl
+++ router_code.erl
@@ -6,7 +6,9 @@
 % 将本地的代码加载到所有其他节点上.
 % -spec nl(Module)-> [{Node, Result}]
 nl(Module)->
-    c:l(Module),
+    %l(Module)
+    code:purge(Module),
+    code:load_file(Module),
     {Module, Binary, Filename} = code:get_object_code(Module),
     AllNodes = gen_server:call(router, get_all_nodes),
     ResultList = router_rpc:multicall(AllNodes, code, load_binary, [Module, Filename, Binary]),
